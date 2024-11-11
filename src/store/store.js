dcec01e4d354f4870f2fdde7ddd741df15887611
Bimbito <rexgui@hotmail.com>
2024-11-11 17:00:03 +0000

    Feat:Redux-Persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, applyMiddleware, legacy_createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import logger from 'redux-logger'
 
 import { rootReducer } from "./root-reducer";
@@ -17,12 +19,22 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next state: ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composeEnchancers = compose(applyMiddleware(...middleWares));
 
 export const store = legacy_createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   composeEnchancers
 );
+
+export const persistor = persistStore(store);
